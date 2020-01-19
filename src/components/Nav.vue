6c7ed4d330f6c18175522f4cbd2ed8ee9fb1efe6
zheng <849859182@qq.com>
2020-01-19 11:49:54 +0800

    Icon组件创建

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,24 +1,40 @@
 <template>
     <div class="nav">
         <router-link to="/"></router-link>
-        <router-link to="/money">记账</router-link>
-        |
-        <router-link to="/labels">标签</router-link>
-        |
-        <router-link to="/Statistics">统计</router-link>
+
+        <router-link to="/money" class="item">
+            <Icon name="money"></Icon>
+            记账
+        </router-link>
+        <router-link to="/labels">
+            <Icon name="label"></Icon>
+            标签
+        </router-link>
+        <router-link to="/Statistics">
+            <Icon name="TaskStatistics"></Icon>
+            统计
+        </router-link>
     </div>
 </template>
 
 <script lang="ts">
+
+
     export default {
         name: "Nav"
     };
 </script>
 
 <style lang="scss" scoped>
-.nav{
-    display: flex;
-    align-items: center;
-    justify-content: space-around;
-}
+    .nav {
+        display: flex;
+        align-items: center;
+        justify-content: space-around;
+    }
+    .item{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
 </style>
