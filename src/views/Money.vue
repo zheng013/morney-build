dfa18fe696a3d5c3b336a5eb56d10cf95913336d
zheng <849859182@qq.com>
2020-02-07 20:55:05 +0800

    完成money视图的四个部分的css，准备进行模块化分类。

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,16 +1,205 @@
 <template>
-        <Layout>
-        <p>111111</p>
-        </Layout>
+    <Layout class-prefix="layout">
+        <div class="tags">
+            <ul class="current">
+                <li>衣</li>
+                <li>食</li>
+                <li>住</li>
+                <li>行</li>
+                <li>衣</li>
+                <li>食</li>
+                <li>住</li>
+                <li>行</li>
+                <li>衣</li>
+                <li>食</li>
+                <li>住</li>
+                <li>行</li>
+            </ul>
+            <div class="new">
+                <button>新增标签</button>
+            </div>
+        </div>
+        <div>
+            <label class="notes">
+                <span class="name">备注</span>
+                <input type="text" placeholder="在这里输入备注">
+            </label>
+        </div>
+        <div>
+            <ul class="types">
+                <li class="selected">支出</li>
+                <li>收入</li>
+            </ul>
+        </div>
+        <div class="numberPad">
+            <div class="output">100</div>
+            <div class="buttons">
+                <button>1</button>
+                <button>2</button>
+                <button>3</button>
+                <button>删除</button>
+                <button>4</button>
+                <button>5</button>
+                <button>6</button>
+                <button>清空</button>
+                <button>7</button>
+                <button>8</button>
+                <button>9</button>
+                <button class="ok">OK</button>
+                <button class="zero">0</button>
+                <button>.</button>
+            </div>                 <!--一个文件超过一百五十行的代码，开始分模块优化-->
+        </div>
+    </Layout>
 </template>
 
 <script lang="ts">
-    export default {
-        name: "Money"
-    };
+  export default {
+    name: "Money"
+  };
 </script>
-
+<style lang="scss">
+    .layout-content {
+        display: flex;
+        flex-direction: column;
+    }
+</style>
 <style lang="scss" scoped>
-        @import "~@/assets/style/helper.scss";
+    @import "~@/assets/style/helper.scss";
 
+    .numberPad {
+        > .output {
+            @extend %innerShadow;
+            font-size: 36px;
+            font-family: Consolas, monospace;
+            padding: 9px 16px;
+            text-align: right;
+        }
+
+        > .buttons {
+            @extend %clearFix;
+
+            > button {
+                float: left; //float需要添加在父元素上添加一个clearfix的类 ::after{content，clear：both，display:block}
+                height: 64px;
+                width: 25%;
+                background: transparent;
+
+                &.ok {
+                    float: right;
+                    height: 64*2px;
+                }
+
+                &.zero {
+                    width: 50%;
+                }
+
+                $bg: #f2f2f2;
+
+                &:nth-child(1) {
+                    background: $bg;
+                }
+
+                &:nth-child(2), &:nth-child(5) {
+                    background: darken($bg, 4%);
+                }
+
+                &:nth-child(3), &:nth-child(6), &:nth-child(9) {
+                    background: darken($bg, 4*2%);
+                }
+
+                &:nth-child(4), &:nth-child(7), &:nth-child(10) {
+                    background: darken($bg, 4*3%);
+                }
+
+                &:nth-child(8), &:nth-child(11), &:nth-child(13) {
+                    background: darken($bg, 4*4%);
+                }
+
+                &:nth-child(14) {
+                    background: darken($bg, 4*5%);
+                }
+
+                &:nth-child(12) {
+                    background: darken($bg, 4*6%);
+                }
+            }
+        }
+    }
+
+    .types {
+        display: flex;
+        background: #c4c4c4;
+        font-size: 24px;
+
+        > li {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            width: 50%;
+            height: 64px;
+            position: relative;
+
+            &.selected::after {
+                content: '';
+                position: absolute;
+                bottom: 0;
+                left: 0;
+                width: 100%;
+                background: #333;
+                height: 4px;
+            }
+        }
+    }
+
+    .notes {
+        font-size: 14px;
+        background: #f5f5f5;
+        display: flex;
+        align-items: center;
+        padding-left: 16px;
+
+        > .name {
+            padding-right: 16px;
+        }
+
+        > input {
+            height: 64px;
+            padding-right: 16px;
+            flex-grow: 1;
+            background: transparent;
+        }
+    }
+
+    .tags {
+        padding: 16px;
+        flex-grow: 1;
+        display: flex;
+        flex-direction: column-reverse;
+        overflow: auto;
+        > .current {
+            display: flex;
+flex-wrap: wrap;
+            > li {
+                background: #d9d9d9;
+                $h: 24px;
+                height: $h;
+                border-radius: $h/2;
+                padding: 0 18px;
+                margin-right: 10px;
+                margin-top:16px;
+            }
+        }
+
+        > .new {
+            padding-top: 16px;
+            order: -1;
+            > button {
+                background: transparent;
+                color: #999;
+                border-bottom: 1px solid;
+                padding: 0 4px
+            }
+        }
+    }
 </style>
