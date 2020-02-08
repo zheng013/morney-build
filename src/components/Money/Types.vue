95fb56d1f48672d4205be603e93e73ba8f211efa
zheng <849859182@qq.com>
2020-02-08 14:23:51 +0800

    使用js实现了一个组件。

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -1,16 +1,31 @@
 <template>
     <div>
         <ul class="types">
-            <li class="selected">支出</li>
-            <li>收入</li>
+            <li :class="type==='-'&&'selected'" @click="typeSelect('-')">支出</li>
+            <li :class="type==='+'&&'selected'" @click="typeSelect('+')">收入</li>
         </ul>
     </div>
 </template>
 
-<script lang="ts">
+<script lang="js">
   export default {
-    name: "types"
-  };
+    name: "types",
+    props: ['xxx'],
+    data() {
+      return {type: '-'}
+    },
+    mounted() {
+      console.log(this.xxx)
+    },
+    methods: {
+      typeSelect(type) {
+        if (type !== '+' && type !== '-') {
+          throw new Error('type is unknown')
+        }
+        this.type = type
+      }
+    }
+  }
 </script>
 
 <style lang="scss" scoped>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -3,16 +3,15 @@
         <!--一个文件超过一百五十行的代码，开始分模块优化-->
         <Tags/>
         <Notes/>
-        <Types/>
+        <Types xxx="hello"/>
         <NumberPad/>
     </Layout>
 </template>
-
 <script lang="ts">
   import Tags from "@/components/Money/Tags.vue";
   import Notes from "@/components/Money/Notes.vue";
-  import Types from '@/components/Money/Types.vue'
-  import NumberPad from '@/components/Money/NumberPad.vue'
+  import Types from "@/components/Money/Types.vue";
+  import NumberPad from "@/components/Money/NumberPad.vue";
 
   export default {
     name: "Money",
