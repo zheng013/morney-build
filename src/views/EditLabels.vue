1ba1398843eaff751d0bdfb21c0f615d817909cc
zheng <849859182@qq.com>
2020-02-12 19:56:12 +0800

    完成LabelEdit组件。

--- src/views/EditLabels.vue
+++ src/views/EditLabels.vue
@@ -7,12 +7,21 @@
 <script lang="ts">
   import Vue from "vue";
   import {Component} from "vue-property-decorator";
+  import Model from "@/models/tagsModel.ts";
 
   @Component
   export default class EditLabels extends Vue {
- created(){
-   console.log(this.$route.params);
- }
+    created() {
+      const id = this.$route.params.id;
+      Model.fetch();
+      const tags = Model.data;
+      const tag = tags.filter(item => item.id === id)[0];
+      if (tag){
+        console.log(tag);
+      }else{
+        this.$router.replace('/404')
+      }
+    }
   }
 </script>
 
